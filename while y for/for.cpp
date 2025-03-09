b56869d9e83b77ff62b16d76f0baa0edd40964f3
JosueHt-00 <jhernandezt17@miumg.edu.gt>
2025-03-08 20:10:22 -0600

    nuevos cambios en programacion

---
+++ while y for/for.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+main(){
+	
+//i++ , i+=1 , i=i+1
+			int inicio = 0,fin = 0,res = 0;
+			
+			cout <<"Ingrese tabla inicio:";
+			cin>>inicio;
+			cout <<"Ingrese tabla fin:";
+			cin>>fin;
+
+			for (int rango=inicio;rango<=fin;rango++){
+			cout<<" tabla del "<<rango<<end1;
+				for (int i=1;i<=10;i++){ 
+				res = rango * i;	
+			 cout<<rango<<" X "<< i <<" = "<<res<<endl;
+			}
+}
+ 
+
+system("pause");
+}
---
+++ while y for/while.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+main(){
+/*
+		int i = 0;
+		while(i<5){
+  		cout<<"while: <<"i<<endl;
+		   i++;
+		   
+}
+*/
+/*
+int ii = 0;
+do {
+	i++;
+  cout <<"do while"<< ii <<endl;
+}while(ii<5);
+  
+
+*/
+
+
+char respuesta;
+
+do{
+	cout<<"Desea Ingresar Otro Valor: ";
+	cin>>respuesta;
+	
+} while (respuesta=='s' || respuesta=='s' );
+
+
+system("pause");
+}
