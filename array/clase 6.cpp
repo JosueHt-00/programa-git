2f80572f5aaeef8abd1a7bebce1987f0861ff9d6
JosueHt-00 <jhernandezt17@miumg.edu.gt>
2025-03-22 13:50:01 -0600

    agrego la clase array

---
+++ array/clase 6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	/* int tam=0,suma=0,promedio = 0;
+	int notas[tam];
+	char res;
+	do{
+		tam++;
+		cout<<"Ingrese la nota "<<tam<<":";
+		cin>>notas[tam-1];
+		cout<<"Desa Ingresar otra nota (s/n):";
+		cin>>res;
+	}while(res=='s' || res=='S');
+   
+	for (int i=0;i<tam ; i++){
+		suma += notas[i];
+	}
+	    promedio = suma / tam;
+	    cout<<"El promedio es:"<<promedio<<endl<<endl;*/
+	
+	/*string nombre; 
+	int dato;
+	cout<<"ingresar Dato :";
+	cin>>dato;
+	cin.ignore();
+	cout<<"Ingrese Nombre Completo:";
+	getline(cin,nombre);
+	
+	cout<<nombre<<endl;*/
+	
+	string semana[7]={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"};
+	for(int i=0;i<7;i++){
+		cout<<semana[i]<<endl;
+	
+	
+	system("pause");
+}
---
+++ array/clase 7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+
+int fil=0,col=0;
+	cout<<"Ingrese Fila:";
+	cin>>fil;
+	cout<<"Ingrese Columanas:";
+	cin>>col;
+	
+	int matriz[fil][col];
+	cout<< "____________ ingresar_______________"<<endl;
+	for (int i=0;i<fil;i++){
+		cout<<i<<","<<ii<<": ";
+	cin>>matriz[i][ii];
+	
+	}	
+	cout<< "___________________________"<<endl;
+	
+	system("pause");
+}
